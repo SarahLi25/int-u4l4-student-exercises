e1229e477181259ffecbbd75f17456a75b2ec2da
SarahLi25 <sarahliwork25@gmail.com>
2025-06-16 10:39:37 -0500

    practice

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let button = document.querySelector("button");
+let result= document.querySelector(".result-text");
 
 button.addEventListener("click", function() {
     let weather = document.querySelector(".weather").value;
     
     // 1. Write a conditional statement:
+
+    if (weather==="rainy") {
+       document.querySelector("p").innerHTML="bring an umbrella"
+    } else {
+        document.querySelector("p").innerHTML="Enjoy the Sun";
+    }
     //  - If the input is "rainy", change the text in the paragraph tag to "Bring an umbrella!"
     //  - Else, change the text in the paragraph tag to "Enjoy the sun!"
 
